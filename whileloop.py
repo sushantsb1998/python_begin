aa0cb3377636b7672163d69096a08e4912ef4824
Sushant Bhattarai <sushantbhattarai@Sushants-MacBook-Pro.local>
2019-12-25 22:43:57 -0800

    big confusion with if else

---
+++ whileloop.py
@@ -0,0 +1,45 @@
+"""
+num = 1
+while num <= 100:
+	print (num)
+	num = num + 1
+
+"""
+"""
+# printing from 500 to 550
+
+num = 500
+while num <= 550:
+	print (num)
+	num = num + 1
+"""
+
+# printin from 50 to 0
+"""
+num =  50
+while num >= 0:
+	print (num)
+	num = num - 1
+"""
+"""
+def ifnum():
+	print(" Enter a number")
+	number = input()
+	if  (number ==  1):
+		print ("1")
+	elif (number == 2):
+		print(" 2 " )
+ifnum()
+"""
+"""
+def ifnum():
+	print("Enter a number:-")
+	number = input()
+	if (number == '1'):
+		print("you have entered number 1")
+	elif(number == '2'):
+		print("you have entered number 2")
+
+ifnum()
+"""
+
