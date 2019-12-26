33c029172b3785b3b9b648125006e2bd3475a244
Sushant Bhattarai <sushantbhattarai@Sushants-MacBook-Pro.local>
2019-12-25 21:34:22 -0800

    adding 2 new files

---
+++ divi5.py
@@ -0,0 +1,12 @@
+#check weather the number is divisable by 5 or not
+
+
+def if_else():
+	print( "enter a number" )
+	num = int ( input())
+	if ( num % 5 == 0 ):
+		print( " It is divisable by 5" )
+	else:
+		print(" It is not divisable by 5")
+if_else()
+
---
+++ newif.py
@@ -0,0 +1,14 @@
+#check weather the number is negative, positive or zero
+
+def if_else():
+	print( "write a number" )
+	num = int ( input())
+	if ( num < 0 ):
+		print( " It is a negative number " )
+	elif ( num == 0):
+		print("it is zero" )
+	else:
+		print(" It is a positive number")
+if_else()
+
+
